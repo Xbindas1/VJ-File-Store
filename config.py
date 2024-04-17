ab10166e604832b51075318824c130c03981262c
Xbindas1 <166252914+Xbindas1@users.noreply.github.com>
2024-04-17 17:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://divine-dyana-xbindas0466.koyeb.app/"
+        URL = "https://xbindas.onrender.com"
     else:
-        URL = "https://divine-dyana-xbindas0466.koyeb.app/"
+        URL = "https://xbindas.onrender.com"
 
 
 
